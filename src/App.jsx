2fcc313abbb8842434d5901f5a9d9f762832e0dd
compileandgo <ioshacker11111@gmail.com>
2026-03-26 23:01:30 +0530

    lkajsdf

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import TextGenerator from "./TextGenerator";
+
+
+const App = () => {
+  return (
+    // <div className="w-full h-full ">
+      <TextGenerator/>
+    // </div>
+  )
+};
+
+export default App;
---
+++ src/TextGenerator.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import useChat from './useChat'
+import ReactMarkDown from "react-markdown"
+import remarkGfm from "remark-gfm"
+
+const TextGenerator = () => {
+    const [input, setInput] = useState("")
+    const {isLoading, generateStream, streamContent, error} = useChat("http://localhost:5000/api/stream");
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (!input.trim()) return;
+        generateStream({message: input})
+        setInput("")
+    }
+
+  return (
+    <div className='w-full h-wull'>
+        <form onSubmit={(e) => handleSubmit(e)}>
+            <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder='Ask something...' required />
+            <button type='submit'>{isLoading ? "loading" : "send"}</button>
+        </form>
+
+        <ReactMarkDown remarkPlugins={[remarkGfm]}>{streamContent}</ReactMarkDown>
+    </div>
+  )
+}
+
+export default TextGenerator
